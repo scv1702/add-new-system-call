4e825e534242e7487fd13542f5f506a4259159c0
Changyu Shin <scv1702@gmail.com>
2022-05-30 18:35:11 +0900

    Initial commit

---
+++ test_hello.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <stdio.h>
+
+#define __NR_hello 436
+
+int main() {
+    int n = 0;
+    n = syscall(__NR_hello, 9, 7);
+    printf("return value: %d\n", n);
+    return 0;
+}
